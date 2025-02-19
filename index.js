709c1f2e2dbe7d061e6ea5c5f5a3167475b05126
Kunyue Xing <scarlett.kunyue.xing@gmail.com>
2025-02-19 12:38:51 -0800

    feat: Created the config environment file. Define the port in index.js

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ const app = require('./app')
 const http = require('http')
 // Create a server with http and the app
 const server = http.createServer(app)
+
+// Define the port, either from the environment which is defined as 3000, or default to 8000
+const port = process.env.PORT || 8000
+// Listen to the port
+server.listen(port, () => {
+    console.log(`App running on port ${port} ...`)
+})
