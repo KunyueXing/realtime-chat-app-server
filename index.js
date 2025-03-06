bbaadf60e706bf83627571ed083e22fc213b8a04
Kunyue Xing <scarlett.kunyue.xing@gmail.com>
2025-03-06 12:10:08 -0800

    Feat: Connect to database using mongoose in server.

--- index.js
+++ index.js
@@ -2,18 +2,34 @@
 // It imports the app from app.js.
 const app = require('./app')
 
+const mongoose = require('mongoose')
+
 // Import the dotenv package to read the .env file and store the environment variables in process.env
 const dotenv = require('dotenv')
 dotenv.config({ path: './config.env' })
 
+// As a good practice, we should not include the password inside the project environment file.
+const database = process.env.DATABASEURI.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+
+// Connect to the database
+mongoose
+  .connect(database)
+  .then(() => {
+    console.log('DB connection successful!')
+  })
+  .catch((err) => {
+    console.log('DB connection failed!')
+    console.log(err)
+  })
+
 // Listener for specific events emitted by the process object, to handle system siganls, errors and life cycle events.
-process.on('uncaughtException', err => {
+process.on('uncaughtException', (err) => {
   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...')
   console.log(err.name, err.message)
   process.exit(1) // Exit code 1 -- there's some issue causing the program to exit
 })
 
-process.on('unhandledRejection', err => {
+process.on('unhandledRejection', (err) => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...')
   console.log(err.name, err.message)
   server.close(() => {
