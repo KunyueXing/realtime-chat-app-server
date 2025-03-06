76843b8cfc8a4501c55a55bc6045a16b7a6e7d45
Kunyue Xing <scarlett.kunyue.xing@gmail.com>
2025-03-06 11:53:24 -0800

    Feat: Added event listeners for process object. Imported the dotenv package to read the .env file.

--- index.js
+++ index.js
@@ -2,6 +2,25 @@
 // It imports the app from app.js.
 const app = require('./app')
 
+// Import the dotenv package to read the .env file and store the environment variables in process.env
+const dotenv = require('dotenv')
+dotenv.config({ path: './config.env' })
+
+// Listener for specific events emitted by the process object, to handle system siganls, errors and life cycle events.
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...')
+  console.log(err.name, err.message)
+  process.exit(1) // Exit code 1 -- there's some issue causing the program to exit
+})
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...')
+  console.log(err.name, err.message)
+  server.close(() => {
+    process.exit(1) // Exit code 1 -- there's some issue causing the program to exit
+  })
+})
+
 // createServer method from the http module.
 // http package is a built-in Node.js package.
 const http = require('http')
