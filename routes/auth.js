7d2e97941f0bac1fad350e9367c025dda3200097
Kunyue Xing <scarlett.kunyue.xing@gmail.com>
2025-03-10 15:30:13 -0700

    Doc: Added comments to auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,8 @@ const authController = require('../controllers/authController')
 router.post('/register', authController.register, authController.sendOTP)
 router.post('/login', authController.login)
 router.post('/verify', authController.verifyOTP)
+// In most applications, this is provided for resending the OTP in case the user didn't receive it the first time.
+// This can be used in the frontend to resend the OTP
 router.post('/send-otp', authController.sendOTP)
 
 router.post('/forgotPassword', authController.forgotPassword)
