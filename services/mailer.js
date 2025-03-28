bb4277b71ea54ee0500589667d6daa22cee842d6
Kunyue Xing <scarlett.kunyue.xing@gmail.com>
2025-03-28 00:00:41 -0700

    Refactor: Finished the mail service and added debug logs

--- services/mailer.js
+++ services/mailer.js
@@ -1,35 +1,44 @@
-const mailgun = require('mailgun.js')
 const dotenv = require('dotenv')
-dotenv.config({ path: '../config.env' })
+dotenv.config({ path: './config.env' })
 
-const mg = mailgun({
-  apiKey: process.env.MAILGUN_API_KEY,
-  domain: process.env.MAILGUN_DOMAIN
+const Mailgun = require('mailgun.js')
+const FormData = require('form-data')
+
+const mailgun = new Mailgun(FormData)
+const mg = mailgun.client({
+  username: 'api',
+  key: process.env.MAILGUN_API_KEY
 })
 
-const sendMailgunEmail = async ({ to, subject, text, otp }) => {
+const sendMailgunEmail = async ({ to, subject, content, html, otp }) => {
   try {
-    const sender = process.env.EMAIL_FROM
+    const sender = process.env.MAILGUN_DOMAIN
+
+    if (!process.env.MAILGUN_API_KEY || !process.env.MAILGUN_DOMAIN) {
+      throw new Error('Mailgun API key or domain is missing in .env file');
+    }
 
     const data = {
       from: `ChatElephant App <no-reply@${sender}>`,
       to: to,
       subject: subject,
-      text: `${text}, the one time password for ${subject} is ${otp}`
+      text: `${content} The one time password is ${otp}`,
+      html: html
     }
 
-    await mg.messages.create(process.env.MAILGUN_DOMAIN, data)
-    console.log(data)
+    const response = await mg.messages.create(process.env.MAILGUN_DOMAIN, data)
+    console.log('Mailgun response:', response)
   } catch (error) {
-    console.log(error)
+    console.error('Error sending email:', error)
   }
 }
 
 exports.sendEmail = async (args) => {
+  console.log('sendEmail() function called with args:', args);
   // If the environment is development, log the email details to the console
   // instead of sending the email. This is useful for testing purposes
   if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-    console.log(args)
+    console.log('Development environment detected. Email not sent.')
     return Promise.resolve()
   } else {
     return sendMailgunEmail(args)
