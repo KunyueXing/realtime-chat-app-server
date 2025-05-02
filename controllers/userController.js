deb2526f1318137322d6ffdf7d1b8efc3e6ebf56
Kunyue Xing <scarlett.kunyue.xing@gmail.com>
2025-05-02 16:45:04 -0700

    feat: addded get users list function and added it to routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ const User = require('../models/user')
 const catchAsync = require('../utils/catchAsync')
 const filterObj = require('../utils/filterObj')
 const AppError = require('../utils/appError')
+const FriendRequest = require('../models/friendRequest')
 
 exports.updateMe = catchAsync(async (req, res, next) => {
   // This user is coming from the protect middleware, which means the user is logged in
@@ -30,5 +31,43 @@ exports.updateMe = catchAsync(async (req, res, next) => {
     data: {
       user: updatedUser
     }
+  })
+})
+
+// Get all users that are not friends with the current user
+exports.getUsers = catchAsync(async (req, res, next) => {
+  // 1) Get all users
+  const all_users = await User.find({ verified: true }).select('firstName lastName _id')
+
+  const curr_user = req.user
+
+  // 2) Filter out the current user and friends, and those who have sent a friend request
+  const not_friends_users = all_users.filter((user) => {
+    // Check if the user is not the current user
+    if (user._id.toString() === curr_user._id.toString()) {
+      return false
+    }
+
+    // Check if the user is not already a friend
+    // Here, .some() populated objects. If use .includes(user_.id), it's quicker but less safer
+    const isFriend = curr_user.friends.some((friend) => friend._id.toString() === user._id.toString())
+    if (isFriend) {
+      return false
+    }
+
+    // Check if the user has sent a friend request to the current user or vice versa
+    // const hasSentRequest = curr_user.friendRequests.some((request) => request.sender._id.toString() === user._id.toString() || request.receiver._id.toString() === user._id.toString())
+    // if (hasSentRequest) {
+    //   return false
+    // }
+
+    return true
+  })
+
+  // 3) Send response
+  res.status(200).json({
+    status: 'success',
+    data: not_friends_users,
+    message: 'Users that are not friends fetched successfully'
   })
 })
